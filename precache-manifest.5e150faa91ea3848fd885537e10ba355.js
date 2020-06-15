322243db9ecb75c60d168c3945335d068559589f
Antoine Hue <antoine.hue@gmail.com>
2020-06-15 22:13:37 +0200

    Updates

---
+++ precache-manifest.5e150faa91ea3848fd885537e10ba355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3267d33aad6663388f4a9ab02808df3e",
+    "url": "/le-vrai-debat/app/index.html"
+  },
+  {
+    "revision": "79971b36a91dc574462c",
+    "url": "/le-vrai-debat/app/static/css/main.3386dd16.chunk.css"
+  },
+  {
+    "revision": "c7adda26e61366f69b10",
+    "url": "/le-vrai-debat/app/static/js/2.5713a561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/le-vrai-debat/app/static/js/2.5713a561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79971b36a91dc574462c",
+    "url": "/le-vrai-debat/app/static/js/main.0e7d0e28.chunk.js"
+  },
+  {
+    "revision": "68aad6d97c07b1e3d4bb",
+    "url": "/le-vrai-debat/app/static/js/runtime-main.1ba6f7e2.js"
+  }
+]);
