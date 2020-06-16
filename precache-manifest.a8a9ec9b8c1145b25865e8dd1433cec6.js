d68187f6553e76fe19fd17b5886b3fa2a54804ce
Antoine Hue <antoine.hue@gmail.com>
2020-06-16 15:36:09 +0200

    Updates

---
+++ precache-manifest.a8a9ec9b8c1145b25865e8dd1433cec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a6df2625c5dd24981be2f530edba2a",
+    "url": "/le-vrai-debat/index.html"
+  },
+  {
+    "revision": "7c737c4b1f36bcb93723",
+    "url": "/le-vrai-debat/static/css/main.ed811831.chunk.css"
+  },
+  {
+    "revision": "e0c7c6ab05d22cbe2f08",
+    "url": "/le-vrai-debat/static/js/2.ec321144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/le-vrai-debat/static/js/2.ec321144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c737c4b1f36bcb93723",
+    "url": "/le-vrai-debat/static/js/main.92468810.chunk.js"
+  },
+  {
+    "revision": "0f5c28fd21c13308083c",
+    "url": "/le-vrai-debat/static/js/runtime-main.508e93ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/le-vrai-debat/precache-manifest.97216cf393a723453ccac76d9b824429.js"
+  "/le-vrai-debat/precache-manifest.a8a9ec9b8c1145b25865e8dd1433cec6.js"
 );
 
 self.addEventListener('message', (event) => {
